0db80fc99374981b16886fd427de58c550432204
sjensen90 <sjense90@gmail.com>
2017-08-29 20:31:14 -0500

    Init with react

---
+++ client/app/SudokuColorApp/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class SudokuColorApp extends React.Component {
+	render() {
+		return (
+			<div>
+				Hello from sudoku
+			</div>
+		)
+	}
+}
--- client/index.js
+++ client/index.js
@@ -1,2 +1,5 @@
-const init = "hello world"
-console.log(init)
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SudokuColorApp from './app/SudokuColorApp'
+
+ReactDOM.render(<App />, document.getElementById('app'))
