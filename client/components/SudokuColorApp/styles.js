81193ee5f41782d5b4118d11e06dd3ad770983b4
Stephen Jensen <stephenjensen@stephens-mbp.attlocal.net>
2019-05-08 20:12:45 -0500

    Fix font styles in production

--- client/components/SudokuColorApp/styles.js
+++ client/components/SudokuColorApp/styles.js
@@ -1,14 +1,13 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
-/* eslint-disable import/prefer-default-export */
 export const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Lato:100,200,300,400,500,600,700&subset=latin-ext');
+
   body {
-    @import url('https://fonts.googleapis.com/css?family=Lato:100,200,300,400,500,600,700&subset=latin-ext');
     font-family: 'Lato', sans-serif;
     font-size: 16px;
   }
 `
-/* eslint-enable import/prefer-default-export */
 
 export const AppContentStyles = styled.div`
   display: flex;
